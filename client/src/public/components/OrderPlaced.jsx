437cff153111f09e21e92a04cfe3c2c73d453305
Abhi <abhiinglepatil02@gmail.com>
2024-02-07 22:25:15 +0530

    orderplaced fixed

--- client/src/public/components/OrderPlaced.jsx
+++ client/src/public/components/OrderPlaced.jsx
@@ -5,7 +5,7 @@ const OrderPlaced = () => {
     return <>
         <div className="container m-5">
             <h3>Hurry!</h3>
-            <h4>Your Order Is Placed Successfully</h4>
+            <h4 className='fw-lighter'>Your Order Is Placed Successfully</h4>
             <Link to={"/myorders"}>My Orders</Link>
         </div>
     </>
