ca666bc1a1d622f3272733fc3ba05db97c1f8dda
Abhi <abhiinglepatil02@gmail.com>
2024-02-07 22:38:11 +0530

    orderplaced fixed

--- client/src/public/components/OrderPlaced.jsx
+++ client/src/public/components/OrderPlaced.jsx
@@ -3,10 +3,18 @@ import { Link } from 'react-router-dom'
 
 const OrderPlaced = () => {
     return <>
-        <div className="container m-5">
-            <h3 className='fw-lighter'>Hurry!</h3>
-            <h4 className='fw-lighter'>Your Order Is Placed Successfully</h4>
-            <Link to={"/myorders"}>My Orders</Link>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-5 mt-5 pt-5 text-center">
+                    <h3 className='fw-bold'>Hurry!</h3>
+                    <h4 className='fw-'>Your Order Is Placed Successfully!</h4>
+                    <p>Payment Id-</p>
+                    <Link to={"/myorders"}>My Orders</Link>
+                </div>
+                <div className="col-sm-7">
+                    <img className=' img-fluid' src="https://img.freepik.com/free-vector/cash-delivery-concept_23-2148786295.jpg?size=626&ext=jpg&ga=GA1.1.499869514.1706805800&semt=ais" alt="" />
+                </div>
+            </div>
         </div>
     </>
 }
