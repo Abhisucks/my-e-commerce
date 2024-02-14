67f7178477dcf9ff3ba7d0a26939440cf6571071
Abhi <abhiinglepatil02@gmail.com>
2024-02-14 22:09:19 +0530

    fixed

--- client/src/public/pages/Contact.jsx
+++ client/src/public/pages/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
         <div class="container py-5">
             <div class="row py-5">
                 <form class="col-md-9 m-auto" method="post" role="form">
-                    <div class="row">
+                    <div class="row" >
                         <div class="form-group col-md-6 mb-3">
                             <label for="inputname">Name</label>
                             <input type="text" class="form-control mt-1" id="name" name="name" placeholder="Name" />
