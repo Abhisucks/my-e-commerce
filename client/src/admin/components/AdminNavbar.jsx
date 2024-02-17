62033d534b45971ee68f8d82653a03fb46f52bd9
Abhi <abhiinglepatil02@gmail.com>
2024-02-17 14:02:15 +0530

    admin navbar fixed

--- client/src/admin/components/AdminNavbar.jsx
+++ client/src/admin/components/AdminNavbar.jsx
@@ -44,9 +44,9 @@ const AdminNavbar = () => {
         <nav class="navbar navbar-expand-lg navbar-light shadow">
             <div class="container d-flex justify-content-between align-items-center">
 
-                <a class="navbar-brand text-success logo h1 align-self-center" href="index.html">
-                    Zay
-                </a>
+                <Link to={"/admin"} class="navbar-brand text-success logo h1 align-self-center" >
+                    Zen
+                </Link>
 
                 <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
                     data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -54,10 +54,11 @@ const AdminNavbar = () => {
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between"
+                <div class=" collapse navbar-collapse "
                     id="templatemo_main_nav">
                     <div class="flex-fill">
-                        <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+
+                        <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto ms-3">
                             <li class="nav-item">
                                 <Link to={"/admin"} class="nav-link" >Dashboard</Link>
                             </li>
@@ -67,78 +68,45 @@ const AdminNavbar = () => {
                             <li class="nav-item">
                                 <Link to={"/admin/users"} class="nav-link" >Users</Link>
                             </li>
-                            {/* <li class="nav-item">
-                                <Link to={"/contact"} class="nav-link" >All-users</Link>
-                            </li> */}
+                            <li class="nav-item">
+                                <Link to={"/admin/messages"} class="nav-link" >Messages</Link>
+                            </li>
                         </ul>
                     </div>
-                    <div class="navbar align-self-center d-flex">
-                        <div class="d-lg-none flex-sm-fill mt-3 mb-4 col-7 col-sm-auto pr-3">
-                            <div class="input-group">
-                                <input type="text" class="form-control" id="inputMobileSearch" placeholder="Search ..." />
-                                <div class="input-group-text">
-                                    <i class="fa fa-fw fa-search"></i>
+
+                    <div class="d-flex mt-2 mt-lg-0 flex-lg-row flex-row-reverse justify-content-end justify-content-lg-start">
+
+                        {login ? (
+                            <div class="ms-lg-4 ms-2">
+                                <div class="dropdown">
+                                    <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown">
+                                        <i class="fa fa-fw fa-user mr-3"></i>
+                                        <span className='ms-1'>{login.name}</span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <button onClick={handleLogout} class="dropdown-item">
+                                                <i class="bi bi-box-arrow-right me-2"></i>
+                                                Logout
+                                            </button>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
-                        </div>
-                        {/* <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal"
-                            data-bs-target="#templatemo_search">
-                            <i class="fa fa-fw fa-search text-dark mr-2"></i>
-                        </a> */}
-                        {/* <a class="nav-icon position-relative text-decoration-none" href="#">
-                            <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
-                            <span
-                                class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span>
-                        </a> */}
-
-                        {
-                            login ? <>
-                                <div className='ms-auto'>
-                                    <div class="dropdown ">
-                                        <button class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" >
-                                            <i class="fa fa-fw fa-user mr-3"></i>
-                                            <span className='ms-1'>{login.name}</span>
-                                        </button>
-                                        <ul class="dropdown-menu">
-                                            <li>
-                                                <button onClick={handleLogout} class="dropdown-item">Logout</button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </>
-                                : <>
-                                    <Link to={"/login"} class="nav-icon position-relative text-decoration-none" >
-                                        <i class="fa fa-fw fa-user text-dark mr-3"></i>
-                                    </Link>
-                                </>
-                        }
+                        ) : (
+                            <Link to={"/login"} class="nav-icon position-relative text-decoration-none text-dark ms-lg-4 ms-2">
+                                <button class="btn btn-outline-dark" type="button">
+                                    <i class="fa fa-fw fa-user mr-3"></i>
+                                    <span className='ms-1'>Login</span>
+                                </button>
+                            </Link>
+                        )}
                     </div>
                 </div>
 
             </div>
         </nav >
         {/* <!-- Close Header --> */}
-
-
-
-        {/* <!-- search Modal --> */}
-        <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="w-100 pt-1 mb-5 text-right">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form action="" method="get" class="modal-content modal-body border-0 p-0">
-                    <div class="input-group mb-2">
-                        <input type="text" class="form-control" id="inputModalSearch" name="q" placeholder="Search ..." />
-                        <button type="submit" class="input-group-text bg-success text-light">
-                            <i class="fa fa-fw fa-search text-white"></i>
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        {/* <!-- Modal --> */}
 
     </>
 }
