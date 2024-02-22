bcd1e212abf6f1ded9b5cedab7defcfc1d68d221
Abhi <abhiinglepatil02@gmail.com>
2024-02-22 23:45:38 +0530

    featuredProduct frontend added

--- client/src/public/components/FeaturedPro.jsx
+++ client/src/public/components/FeaturedPro.jsx
@@ -1,95 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllProduct } from '../../redux/actions/adminActions'
 
 const FeaturedPro = () => {
+    const dispatch = useDispatch()
+    const { allProducts, loading: productLoading, error: productError } = useSelector(state => state.admin)
+
+    useEffect(() => {
+        dispatch(getAllProduct())
+    }, [])
+
+    // Filter only the featured products
+    const featuredProducts = allProducts && allProducts.filter(product => product.featured);
+    console.log(featuredProducts);
+
     return <>
-        <section class="bg-light">
-            <div class="container py-5">
-                <div class="row text-center py-3">
-                    <div class="col-lg-6 m-auto">
-                        <h1 class="h1">Featured Product</h1>
+        <section className="bg-light">
+            <div className="container py-5">
+                <div className="row text-center py-3">
+                    <div className="col-lg-6 m-auto">
+                        <h1 className="h1">Featured Product</h1>
                         <p>
                             Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                             Excepteur sint occaecat cupidatat non proident.
                         </p>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12 col-md-4 mb-4">
-                        <div class="card h-100">
-                            <a href="shop-single.html">
-                                <img src="https://img.freepik.com/free-photo/different-vegetables-seeds-fruits-table-healthy-diet-flat-lay-top-view_1150-42611.jpg?size=626&ext=jpg&ga=GA1.1.499869514.1706805800&semt=ais" class="card-img-top" alt="..." />
-                            </a>
-                            <div class="card-body">
-                                <ul class="list-unstyled d-flex justify-content-between">
-                                    {/* <li>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-muted fa fa-star"></i>
-                                        <i class="text-muted fa fa-star"></i>
-                                    </li> */}
-                                    <li class="text-muted text-right">₹240.00</li>
-                                </ul>
-                                <a href="shop-single.html" class="h2 text-decoration-none text-dark">Gym Weight</a>
-                                <p class="card-text">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia
-                                    deserunt.
-                                </p>
-                                <p class="text-muted">See More</p>
+                <div className="row">
+                    {featuredProducts && featuredProducts.map(product => (
+                        <div key={product._id} className="col-12 col-md-4 mb-4">
+                            <div className="card h-100">
+                                <a href="shop-single.html">
+                                    <img src={product.img} className="card-img-top" alt="..." />
+                                </a>
+                                <div className="card-body">
+                                    <ul className="list-unstyled d-flex justify-content-between">
+                                        {/* Rating Stars */}
+                                        <li>
+                                            <i className="text-warning fa fa-star"></i>
+                                            <i className="text-warning fa fa-star"></i>
+                                            <i className="text-warning fa fa-star"></i>
+                                            <i className="text-muted fa fa-star"></i>
+                                            <i className="text-muted fa fa-star"></i>
+                                        </li>
+                                        <li className="text-muted text-right">{`₹${product.price.toFixed(2)}`}</li>
+                                    </ul>
+                                    <a href="shop-single.html" className="h2 text-decoration-none text-dark">{product.title}</a>
+                                    <p className="card-text">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga perferendi
+                                    </p>
+                                    <p className="text-muted">See More</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-12 col-md-4 mb-4">
-                        <div class="card h-100">
-                            <a href="shop-single.html">
-                                <img src="https://img.freepik.com/premium-photo/high-angle-view-food-table_1048944-4398612.jpg?size=626&ext=jpg&ga=GA1.1.499869514.1706805800&semt=ais" class="card-img-top" alt="..." />
-                            </a>
-                            <div class="card-body">
-                                <ul class="list-unstyled d-flex justify-content-between">
-                                    <li>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-muted fa fa-star"></i>
-                                        <i class="text-muted fa fa-star"></i>
-                                    </li>
-                                    <li class="text-muted text-right">$480.00</li>
-                                </ul>
-                                <a href="shop-single.html" class="h2 text-decoration-none text-dark">Cloud Nike Shoes</a>
-                                <p class="card-text">
-                                    Aenean gravida dignissim finibus. Nullam ipsum diam, posuere vitae pharetra sed, commodo
-                                    ullamcorper.
-                                </p>
-                                <p class="text-muted">Reviews (48)</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4 mb-4">
-                        <div class="card h-100">
-                            <a href="shop-single.html">
-                                <img src="https://img.freepik.com/free-photo/top-view-fresh-foods-spices-vegetables-bottom-white-table_140725-142722.jpg?size=626&ext=jpg&ga=GA1.1.499869514.1706805800&semt=ais" class="card-img-top" alt="..." />
-                            </a>
-                            <div class="card-body">
-                                <ul class="list-unstyled d-flex justify-content-between">
-                                    <li>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-warning fa fa-star"></i>
-                                        <i class="text-warning fa fa-star"></i>
-                                    </li>
-                                    <li class="text-muted text-right">$360.00</li>
-                                </ul>
-                                <a href="shop-single.html" class="h2 text-decoration-none text-dark">Summer Addides
-                                    Shoes</a>
-                                <p class="card-text">
-                                    Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et
-                                    scelerisque ipsum lobortis nec.
-                                </p>
-                                <p class="text-muted">Reviews (74)</p>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
